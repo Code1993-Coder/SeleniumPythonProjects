77fd98b2eac36f05d22eeb16816c7437c17c9235
Manju Mohan <manjumohan290@gmail.com>
2026-01-08 14:29:19 +0530

    second commit

--- firstpython.py
+++ firstpython.py
@@ -1,1 +1,17 @@
-print("my first python program")
+class Program():
+
+
+    def __init__(self):
+        print('This will get executed first')
+
+
+
+    def test_program(self):
+        print('This will get executed after constructor is initialised')
+
+
+
+
+
+prog=Program()
+prog.test_program()
